73b19d40052027afe8e8a7aa2f10b9b4a0dada46
Shashwat <shashwatratna@gmail.com>
2024-04-18 20:51:16 +0530

    Add project files.

---
+++ Basidomycota/Basidomycota.cpp
@@ -0,0 +1,12 @@
+﻿// Basidomycota.cpp : Defines the entry point for the application.
+//
+
+#include "Basidomycota.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
